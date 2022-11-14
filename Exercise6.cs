7ab910ee4d52c4eadc237591777ad83a27aeb769
duaa-braik <duaa13braik@gmail.com>
2022-11-14 12:20:02 +0200

    finished the sixth and sventh problems

---
+++ Exercise6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// link to the problem : https://leetcode.com/problems/rotate-string/
+
+/* Description: 
+ * Given two strings s and goal, return true if and only if s can become goal after some
+ * number of shifts on s.
+ * A shift on s consists of moving the leftmost character of s to the rightmost position.
+ * For example, if s = "abcde", then it will be "bcdea" after one shift.
+ */
+
+public class Exercise6
+{
+    public bool RotateString(string s, string goal)
+    {
+        string leftMost = "";
+        int length = s.Length;
+        bool equals = false;
+        for (int i = 0; i < length; i++)
+        {
+            leftMost = s[0].ToString();
+            s = s.Remove(0, 1);
+            s += leftMost;
+            if (s == goal)
+            {
+                equals = true;
+                break;
+            }
+        }
+        return equals;
+    }
+}
---
+++ Exercise7.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// link to the problem : https://leetcode.com/problems/reverse-words-in-a-string/
+
+/* Description: 
+ * 
+ * Given an input string s, reverse the order of the words.
+ * A word is defined as a sequence of non-space characters. 
+ * The words in s will be separated by at least one space.
+ * Return a string of the words in reverse order concatenated by a single space.
+ * Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+ */
+
+namespace Stack
+{
+    using System.Collections.Generic;
+    public class Exercise7
+    {
+        public string ReverseWords(string s)
+        {
+            s = s.Trim();
+            string[] words = s.Split(' ');
+            Stack<string> stack = new Stack<string>();
+            Console.WriteLine(words.Length);
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i].Length);
+                if (words[i].Length > 0)
+                    stack.Push(words[i]);
+            }
+            s = "";
+            while (stack.Count > 0)
+            {
+                s += stack.Pop();
+                if (stack.Count > 0)
+                    s += " ";
+            }
+            return s;
+        }
+    }
+}
